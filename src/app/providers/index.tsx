b86daebc25f1c95ca5fd1a10a6b4720c0f6f079a
Ethan <ethan@zolplay.com>
2024-02-18 22:22:28 +0800

    feat: rm next themes

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,16 +1,8 @@
 "use client";
 
 import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
 import { ZunstandProvider } from "./zustand";
 
-export function Provider({ children }: ThemeProviderProps) {
-  return (
-    <ZunstandProvider>
-      <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
-        {children}
-      </NextThemesProvider>
-    </ZunstandProvider>
-  );
+export function Provider({ children }: { children: React.ReactNode }) {
+  return <ZunstandProvider>{children}</ZunstandProvider>;
 }
