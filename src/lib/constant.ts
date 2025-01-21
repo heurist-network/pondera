b5a8051529352b509a50b2074854420a17a34bbc
Ethan <ethan@zolplay.com>
2025-01-21 10:21:46 +0800

    fix: title generate

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,7 +1,7 @@
 export const BASE_PROMPT = 'You are a helpful AI assistant.'
 
 export const GENERATE_CHAT_NAME_PROMPT =
-  'What would be a short and relevant title for this chat? You must strictly answer with the title only, no other text is allowed. Use no more than 5 English words.'
+  'What would be a short and relevant title for this chat? You must strictly answer with only the title, no other text is allowed.\n\nUse no more than 5 English words.'
 
 export const PROMPT_STORE = [
   {
